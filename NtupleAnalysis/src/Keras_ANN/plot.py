1ea3d1a09309b200ff3d9a08adb46d06fa1652df
Sotiroulla Konstantinou <skonstan@cern.ch>
2019-11-20 07:21:58 -0600

    Added Keras_ANN directory. It includes codes to train a Keras NN top-tagger and plotting scripts

---
+++ NtupleAnalysis/src/Keras_ANN/plot.py
@@ -0,0 +1,227 @@
+import ROOT
+import array
+import os
+import getpass
+
+def CreateCanvas():
+    canvas = ROOT.TCanvas()
+    return canvas
+
+def CreateLegend(xmin=0.55, ymin=0.75, xmax=0.85, ymax=0.85):
+    leg = ROOT.TLegend(xmin, ymin, xmax, ymax)
+    leg.SetFillStyle(0)
+    leg.SetBorderSize(0)
+    leg.SetTextSize(0.040)
+    leg.SetTextFont(42)
+    #leg.SetTextSize(0.09)
+    leg.SetLineColor(1)
+    leg.SetLineStyle(1)
+    leg.SetLineWidth(1)
+    leg.SetFillColor(0)
+    return leg
+
+def Text(text, x=0.3, y=0.8):
+    tex = ROOT.TLatex(x, y, text)
+    tex.SetNDC()
+    tex.SetTextAlign(31)
+    tex.SetTextFont(42)
+    tex.SetTextSize(0.030)
+    tex.SetLineWidth(2)
+    return tex
+
+def CreateGraph(gx, gy):
+    graph=ROOT.TGraph(2, array.array("d",gx), array.array("d",gy)) 
+    graph.SetFillColor(1)
+    graph.SetLineColor(ROOT.kBlack)
+    graph.SetLineStyle(3)
+    graph.SetLineWidth(2)
+    return graph
+
+def GetGraph(x, y, xerrl, xerrh, yerrl, yerrh):
+    graph = ROOT.TGraphAsymmErrors(len(x), array.array("d",x), array.array("d",y),
+                                   array.array("d",xerrl), array.array("d",xerrh),
+                                   array.array("d",yerrl), array.array("d",yerrh))
+    return graph
+
+def GetRatioStyle(h_ratio, ytitle, xtitle, ymax=2, ymin=0):
+    h_ratio.SetMaximum(ymax)
+    h_ratio.SetMinimum(ymin)
+    h_ratio.GetYaxis().SetTitleOffset(0.5)
+    h_ratio.SetTitle("")
+    h_ratio.GetYaxis().SetTitle(ytitle)
+    h_ratio.GetXaxis().SetTitle(xtitle)
+    h_ratio.GetYaxis().SetLabelSize(0.09)
+    h_ratio.GetXaxis().SetLabelSize(0.09)
+    h_ratio.GetYaxis().SetTitleSize(0.095)
+    h_ratio.GetXaxis().SetTitleSize(0.095)
+    h_ratio.GetXaxis().SetTickLength(0.08)
+    h_ratio.GetYaxis().SetTitleOffset(0.5)
+    return h_ratio
+
+def getDirName(dirName, baseDir=None):
+    print "getDirName() => FIXME! Do not assume full save path"
+    usrName = getpass.getuser() 
+    usrInit = usrName[0]
+    dirName = dirName.replace(".", "p")
+    dirName = "/afs/cern.ch/user/%s/%s/public/html/%s" % (usrInit, usrName, dirName)
+    return dirName
+
+def CreateDir(saveDir):
+    # Create output directory if it does not exist
+    if not os.path.exists(saveDir):
+        os.mkdir(saveDir)
+        if 0:
+            print "Directory " , saveDir ,  " has been created "
+    else:
+        if 0:
+            print "Output saved under", saveDir
+    return
+
+def SavePlot(canvas, saveDir, saveName, saveFormats=["pdf", "root"], verbose=True):
+    
+    # Create output directory if it does not exist
+    CreateDir(saveDir)
+
+    savePath = "%s/%s" % (saveDir, saveName)
+    usrInit =  getpass.getuser()[0]
+    saveURL  = savePath.replace("/afs/cern.ch/user/%s/" %(usrInit),"https://cmsdoc.cern.ch/~")
+    saveURL  = saveURL.replace("/public/html/","/")
+    
+    for ext in saveFormats:
+        fileName = "%s.%s" % (savePath, ext)
+        canvas.SaveAs( fileName )
+        if verbose:
+            print "=== ","%s.%s" % (saveURL, ext)
+    return
+
+def ApplyStyle(h, color):
+    h.SetLineColor(color)
+    h.SetMarkerColor(color)
+    h.SetMarkerStyle(8)
+    h.SetMarkerSize(0.5)
+    h.SetLineWidth(3)
+    h.SetTitle("")
+    return
+
+
+def GetHistoInfo(var):
+    h = var.lower()
+    xmin = 0
+    xmax = 1000
+    units = ""
+    xlabel = var
+    width   = 10 # binwidth
+    _format = "%0.0f "
+
+    if "mass" in h:
+        units   = "GeV/c^{2}"
+        xlabel  = "M (%s)" % units
+        _format = "%0.0f "
+        width   = 10
+
+    if "trijetmass" in h:
+        units  = "GeV/c^{2}"
+        xlabel = "m_{top} (%s)" % units
+        xmax = 805 #1005
+
+    if "dijetmass" in h:
+        xlabel = "m_{W} (%s)" % units
+        xmax   = 600
+        width  = 10
+
+    if "bjetmass" in h:
+        xlabel = "m_{b-tagged jet} (%s)" % units
+        width = 1
+        xmax = 50
+
+    if "bjetldgjetmass" in h:
+        xlabel = "m_{b, ldg jet} (%s)" % units
+        xmax = 705
+
+    if "bjetsubldgjetmass" in h:
+        xlabel = "m_{b-tagged, subldg jet} (%s)" % units
+        xmax = 705
+
+    if "jet_mass" in h:
+        xmax = 750
+
+    if "mult" in h:
+        _format = "%0.0f "
+        width = 1
+        xmax = 50
+        if "ldg" in h:
+            xlabel = "Leading jet mult"
+        if "subldg" in h:
+            xlabel = "Subleading jet mult"
+
+    if "cvsl" in h:
+        _format = "%0.2f "
+        width  = 0.01
+        xmax = 1
+        xmin = -1
+        if "ldg" in h:
+            xlabel = "Leading jet CvsL"
+        if "subldg" in h:
+             xlabel = "Subleading jet CvsL"
+
+    if "axis2" in h:
+        _format = "%0.3f "
+        width = 0.004
+        xmax = 0.2
+        if "ldg" in h:
+            xlabel = "Leading jet axis2"
+        if "subldg" in h:
+            xlabel = "Subleading jet axis2"
+
+    if "trijetptdr" in h:
+        xmax =800
+        _format = "%0.0f "
+        xlabel = "p_{T}#Delta R_{t}"
+
+    if "dijetptdr" in h:
+        xmax =800
+        _format = "%0.0f "
+        xlabel = "p_{T}#Delta R_{W}"
+
+    if "dgjetptd" in h:
+        _format = "%0.2f "
+        xlabel = "Leading jet p_{T}D"
+        width = 0.01
+        xmax = 1
+        if "subldg" in h:
+            xlabel = "Subleading jet p_{T}D"
+
+    if "bdisc" in h:
+        _format = "%0.2f "
+        width   = 0.01
+        xmax = 1
+        if "subldg" in h:
+            xlabel = "Subleading jet CSV"
+        elif "ldg" in h:
+            xlabel = "Leading jet CSV"
+        else:
+            xlabel = "b-tagged jet CSV"
+
+    if "softdrop" in h:
+        xmax = 2
+        width = 0.04
+        _format = "%0.2f "
+        xlabel = "SoftDrop_n2"
+    if not (units == ""):
+        units = " (%s)" % units
+        
+    nbins = (xmax - xmin) / width
+    nbins = int(nbins)
+
+    _format = _format % width + units
+    ylabel = "Arbitrary Units / " + _format
+    info     = {
+        "xmin"   : xmin,
+        "xmax"   : xmax,
+        "nbins"  : nbins,
+        "units"  : units,
+        "xlabel" : xlabel,
+        "ylabel" : ylabel,
+    }
+    return info
+
---
+++ NtupleAnalysis/src/Keras_ANN/pretrainANN.py
@@ -0,0 +1,204 @@
+#!/usr/bin/env python
+'''
+DESCRIPTION:
+This script is used to control the performance of the mass-decorrelated top tagger.
+The program defines two Keras model: a classifier and a regressor. The classifier is trained for a number of epochs. Using a testing sample, its output is predicted and given as input to the regressor. The output of the regressor is a prediction of the top-quark mass.
+This script is used to check that both classifier and regressor are normally trained.
+If one of them does not work, it is easy to detect and fix the problem (change the activation function, optimizer, number of layers, number of neurons etc)
+
+EXAMPLE: 
+./pretrainANN.py --act1 relu --act2 relu --act3 relu --act4 tanh --act5 relu --opt Adam
+'''
+import keras
+import uproot
+import numpy
+import pandas
+import ROOT
+import array
+import os
+import math
+import json
+import random as rn
+import tensorflow as tf
+from keras.models import Sequential, Model
+from keras.layers import Dense, Activation, Flatten, Input
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from sklearn.externals import joblib
+import keras.backend as K
+
+from sklearn.metrics import roc_auc_score
+from scipy.stats import pearsonr
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' #Disable AVX/FMA Warning
+
+#Run in single CPU: this ensures reproducible results!
+config = tf.ConfigProto(intra_op_parallelism_threads=1, inter_op_parallelism_threads=1,
+                        allow_soft_placement=True, device_count = {'CPU': 1})
+session = tf.Session(config=config)
+K.set_session(session)
+
+ACT1 = 'relu'
+ACT2 = 'relu'
+ACT3 = 'sigmoid'
+ACT4 = 'sigmoid'
+ACT5 = 'sigmoid'
+OPT = 'adam'
+from optparse import OptionParser
+parser = OptionParser(usage="Usage: %prog [options]")
+parser.add_option("--act1",dest="act1",  default=ACT1,   help="Activation function 1 (Default: %s)" % ACT1)
+parser.add_option("--act2",dest="act2",  default=ACT2,   help="Activation function 2 (Default: %s)" % ACT2)
+parser.add_option("--act3",dest="act3",  default=ACT3,   help="Activation function 3 (Default: %s)" % ACT3)
+parser.add_option("--act4",dest="act4",  default=ACT4,   help="Activation function 2 (Default: %s)" % ACT4)
+parser.add_option("--act5",dest="act5",  default=ACT5,   help="Activation function 3 (Default: %s)" % ACT5)
+parser.add_option("--opt",dest="opt",  default=OPT,   help="Regressor: Optimizer (Default: %s)" % OPT)
+opt, args = parser.parse_args()
+
+def getInputs():
+    inputList = []
+    inputList.append("TrijetPtDR")
+    inputList.append("TrijetDijetPtDR")
+    inputList.append("TrijetBjetMass")
+    inputList.append("TrijetLdgJetBDisc")
+    inputList.append("TrijetSubldgJetBDisc")
+    inputList.append("TrijetBJetLdgJetMass")
+    inputList.append("TrijetBJetSubldgJetMass")
+    inputList.append("TrijetMass")
+    inputList.append("TrijetDijetMass")
+    inputList.append("TrijetBJetBDisc")
+    inputList.append("TrijetSoftDrop_n2")
+    inputList.append("TrijetLdgJetCvsL")
+    inputList.append("TrijetSubldgJetCvsL")
+    inputList.append("TrijetLdgJetPtD")
+    inputList.append("TrijetSubldgJetPtD")
+    inputList.append("TrijetLdgJetAxis2")
+    inputList.append("TrijetSubldgJetAxis2")
+    inputList.append("TrijetLdgJetMult")
+    inputList.append("TrijetSubldgJetMult")
+    return inputList
+
+def make_trainable(model, isTrainable):
+    model.trainable = isTrainable
+    for l in model.layers:
+        l.trainable = isTrainable
+
+def make_loss_D(c):
+    def loss_D(y_true, y_pred):
+        return c * K.binary_crossentropy(y_true, y_pred)
+    return loss_D
+
+def make_loss_R(c):
+    def loss_R(z_true, z_pred):
+        return c * keras.losses.mean_squared_logarithmic_error(z_true, z_pred)
+    return loss_R
+
+#Main
+# Definitions
+filename = "histograms-TT_19var.root"
+tfile    = ROOT.TFile.Open(filename)
+debug    = 0
+seed     = 1234
+nepochs  = 5000
+nbatch   = 100
+lam = 10
+
+print "Classifier: Activations: %s_%s_%s_sigmoid" % (opt.act1, opt.act2, opt.act3)
+print "Regressor:  Activations: %s_%s" % (opt.act5, opt.act4)
+print "Regressor:  OptimizerL %s" % (opt.opt)
+if 0:
+    nepochs = 10
+    nbatch = 5000
+
+# Setting the seed for numpy-generated random numbers
+numpy.random.seed(seed)
+# Setting the seed for python random numbers
+rn.seed(seed)
+# Setting the graph-level random seed.
+tf.set_random_seed(seed)
+
+#Signal and background branches
+signal     = uproot.open(filename)["treeS"]
+background = uproot.open(filename)["treeB"]
+
+# Input list
+inputList = getInputs()
+nInputs = len(inputList)
+
+#Signal and background dataframes
+df_signal     = signal.pandas.df(inputList)
+df_background = background.pandas.df(inputList)
+
+nsignal = len(df_signal.index)
+print "=== Number of signal events: ", nsignal
+
+# Concat signal, background datasets
+df_signal = df_signal.assign(signal=1)
+df_background = df_background.assign(signal=0)
+
+#Signal and background datasets
+dset_signal     = df_signal.values
+dset_background = df_background.values
+#
+X_signal     = dset_signal[:nsignal, 0:nInputs]
+X_background = dset_background[:nsignal, 0:nInputs]
+
+dataset = pandas.concat([df_signal, df_background]).values
+dataset_target_all = pandas.concat([signal.pandas.df(["TrijetMass"]), background.pandas.df(["TrijetMass"])]).values
+#Split data into input (X) and output (Y)
+X = dataset[:2*nsignal,0:nInputs]
+Y = dataset[:2*nsignal,nInputs:]
+target = dataset_target_all[:2*nsignal, :]
+
+numpy.random.seed(seed)
+
+# Define keras models
+
+# Get training and test samples
+X_train, X_test, Y_train, Y_test, target_train, target_test = train_test_split(
+    X, Y, target, test_size=0.5, random_state=seed, shuffle=True)
+
+
+inputs = Input(shape=(X_train.shape[1],))
+Dx = Dense(32, activation=opt.act1)(inputs)
+Dx = Dense(32, activation=opt.act2)(Dx)
+Dx = Dense(32, activation=opt.act3)(Dx)
+Dx = Dense(1, activation='sigmoid')(Dx)
+D = Model(inputs=[inputs], outputs=[Dx])
+D.compile(loss="binary_crossentropy", optimizer="adam")
+
+print "=== D model: Fit"
+D.fit(X_train, Y_train, validation_data=(X_test, Y_test), epochs=10, verbose=1)
+
+y_predD = D.predict(X_test)
+for i in range(20):
+    print("Predicted=%s (%s)"% (target_test[i], y_predD[i]))
+
+###################################################
+#Pretraining
+###################################################
+
+inputs = Input(shape=(X_train.shape[1],))
+
+Dx = Dense(32, activation=opt.act1)(inputs)
+Dx = Dense(32, activation=opt.act2)(Dx)
+Dx = Dense(32, activation=opt.act3)(Dx)
+Dx = Dense(1, activation='sigmoid')(Dx)
+D = Model(inputs=[inputs], outputs=[Dx])
+
+inp_target = Input(shape=(y_predD.shape[1],))
+Rx = Dense(16, activation=opt.act4)(inp_target) #epochs = 10, dense = 16
+Rx = Dense(1, activation = opt.act5)(Rx)
+R = Model(inputs=[inp_target], outputs=[Rx])
+
+print "===D model"
+D.summary()
+print "===R model"
+R.summary()
+R.compile(loss="msle", optimizer=opt.opt)
+R.fit(Y_train, target_train, validation_data=(Y_test, target_test), epochs=5, verbose=1)
+
+#y_pred = R.predict(y_predD)
+#for i in range(20):
+#    print("Predicted=%.3f (%.3f) (%.3f) (%.3f)"% (target_test[i], y_pred[i], Y_test[i], y_predD[i]))
+
+
