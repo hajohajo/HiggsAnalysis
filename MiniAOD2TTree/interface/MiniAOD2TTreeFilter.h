67ef0279a73e412db5d7d068a1dfe441fee5625b
attikis@cern.ch <attikis@cern.ch>
2020-01-29 07:14:20 -0600

    [Skimming]: Disabled obsolete and redundant softBTag dumper. Added optional debugging cout

--- MiniAOD2TTree/interface/MiniAOD2TTreeFilter.h
+++ MiniAOD2TTree/interface/MiniAOD2TTreeFilter.h
@@ -25,7 +25,7 @@
 #include "HiggsAnalysis/MiniAOD2TTree/interface/ElectronDumper.h"
 #include "HiggsAnalysis/MiniAOD2TTree/interface/MuonDumper.h"
 #include "HiggsAnalysis/MiniAOD2TTree/interface/JetDumper.h"
-#include "HiggsAnalysis/MiniAOD2TTree/interface/SoftBTagDumper.h"
+// #include "HiggsAnalysis/MiniAOD2TTree/interface/SoftBTagDumper.h"
 #include "HiggsAnalysis/MiniAOD2TTree/interface/TopDumper.h"
 #include "HiggsAnalysis/MiniAOD2TTree/interface/METDumper.h"
 #include "HiggsAnalysis/MiniAOD2TTree/interface/GenMETDumper.h"
@@ -74,7 +74,7 @@ class MiniAOD2TTreeFilter : public edm::EDFilter {
 	std::vector<edm::ParameterSet> electronCollections;
 	std::vector<edm::ParameterSet> muonCollections;
 	std::vector<edm::ParameterSet> jetCollections;
-	std::vector<edm::ParameterSet> softBTagCollections;
+	// std::vector<edm::ParameterSet> softBTagCollections;
         std::vector<edm::ParameterSet> topCollections;
 	std::vector<edm::ParameterSet> metCollections;
 	std::vector<edm::ParameterSet> genMetCollections;
@@ -95,7 +95,7 @@ class MiniAOD2TTreeFilter : public edm::EDFilter {
 	ElectronDumper* electronDumper;
 	MuonDumper* muonDumper;
 	JetDumper* jetDumper;
-	SoftBTagDumper* softBTagDumper;
+	// SoftBTagDumper* softBTagDumper;
         TopDumper* topDumper;
 	METDumper* metDumper;
 	GenMETDumper* genMetDumper;
